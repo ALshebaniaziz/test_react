c67c5a28b1507e64eb7f5baa6555e9537217ac42
ALshebaniaziz <143352511+ALshebaniaziz@users.noreply.github.com>
2024-03-24 06:19:43 +0200

    first

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
+const API_URL = 'https://reqres.in/api/login';
+
+const App = () => {
+  const [accessToken, setAccessToken] = useState('');
+
+  useEffect(() => {
+    sendLoginRequest();
+  }, []);
+
+  const sendLoginRequest = async () => {
+    try {
+      const response = await axios.post(API_URL, {
+        
+        RegNo: '112181502',
+        Password: '6504104',
+      });
+
+      setAccessToken(response.data.access_token);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Access Token: {accessToken}</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
